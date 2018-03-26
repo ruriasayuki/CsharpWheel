fb2b623a42b49d7a219bb71e50f3140b7aa52f5b
RuriAsayuki <ruriasayuki@outlook.com>
2018-03-26 09:44:06 +0800

    DataGridHelper
    可以用Attribute来设定 是否要讲成员显示在DataGridView里面

---
+++ DataGridHelper/DataArr.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asayuki.Common
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    class DataArr : System.Attribute
+    {
+        private bool _flag;
+        public DataArr()
+        {
+            _flag = true;
+        }
+        public bool flag
+        {
+            get{return _flag;}
+            set{_flag = value;}
+        }
+    }
+}
---
+++ DataGridHelper/DataGridHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asayuki.Common
+{
+    class DataGridHelper
+    {
+        public void addAttrColumns(Type someType, DataGridView dataGridView)
+        {
+            IList<string> AttrProp = new List<string>();
+
+            MemberInfo[] info = someType.GetMembers();
+            for (int i = 0; i < info.Length; i++)
+            {
+                object[] attributes = info[i].GetCustomAttributes(true);
+                for (int j = 0; j < attributes.Length; j++)
+                {
+
+                    if (attributes[j].ToString() == "JXGY.Common.DataArr")
+                    {
+                        AttrProp.Add(info[i].ToString());
+                        break;
+                    }
+                }
+            }
+            foreach (string a in AttrProp)
+            {
+                DataGridViewColumn column = new DataGridViewTextBoxColumn();
+                column.DataPropertyName = a.ToString().Split(' ')[1];
+                column.Name = a.ToString().Split(' ')[1];
+                dataGridView.Columns.Add(column);
+            }
+
+        }
+    }
+}
