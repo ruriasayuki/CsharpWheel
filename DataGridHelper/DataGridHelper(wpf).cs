360613cb7cd9c1c84e9aedc5058702ab3c7f1b41
RuriAsayuki <ruriasayuki@outlook.com>
2018-05-02 16:59:00 +0800

    增加了wpf支持

--- DataGridHelper/DataGridHelper(wpf).cs
+++ DataGridHelper/DataGridHelper(wpf).cs
@@ -1,19 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace Asayuki.Common
 {
     class DataGridHelper
     {
-        public void addAttrColumns(Type someType, DataGridView dataGridView)
+        public void addAttrColumns(Type someType, DataGrid dataGridView)
         {
             addAttrColumns(someType, dataGridView, 1);
 
         }
-        public void addAttrColumns(Type someType, DataGridView dataGridView,int priority)
+        public void addAttrColumns(Type someType, DataGrid dataGridView,int priority)
         {
             IList<string> AttrProp = new List<string>();
 
@@ -36,9 +39,10 @@ namespace Asayuki.Common
                 int cp = int.Parse(a.ToString().Split('_')[3]);
                 if (cp <= priority)
                 {
-                    DataGridViewColumn column = new DataGridViewTextBoxColumn();
-                    column.DataPropertyName = a.ToString().Split('_')[0].Split(' ')[1];
-                    column.Name = a.ToString().Split('_')[1];
+                    DataGridTextColumn column = new DataGridTextColumn();
+                    column.IsReadOnly = true;
+                    column.Binding = new Binding(a.ToString().Split('_')[0].Split(' ')[1]);
+                    column.Header = a.ToString().Split('_')[1];
                     column.Width = int.Parse(a.ToString().Split('_')[2]);
                     dataGridView.Columns.Add(column);
                 }
